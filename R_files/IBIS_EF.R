929556def95017abd6f1becfcf51a96c107250c9
Neva Corrigan <nevao@uw.edu>
2024-11-05 13:07:30 -0800

    initial commit

---
+++ R_files/IBIS_EF.R
@@ -0,0 +1,6 @@
+
+
+rm(list = ls())
+
+ibis_behav <- read.csv(file.path("/Users/neva/Documents/IBIS Executive Function/dataframes and plots/",
+                                  "IBIS_behav_dataframe_demographics_AnotB_Flanker_DCCS.csv"))
