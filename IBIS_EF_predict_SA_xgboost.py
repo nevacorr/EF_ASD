b9ed20f196738b729ae8a63c28c3c406b33544aa
Neva <nevao@uw.edu>
2025-03-07 16:03:22 -0800

    Plot feature correlation heatmap and move functions to separate file

--- IBIS_EF_predict_SA_xgboost.py
+++ IBIS_EF_predict_SA_xgboost.py
@@ -1,34 +1,33 @@
 import pandas as pd
 import os
 from pycaret.regression import setup, create_model, tune_model, finalize_model, predict_model, save_model, load_model
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
+from Utility_Functions_XGBoost import load_and_clean_data, plot_correlations
 
-output_to_predict = "Flanker_Standard_Age_Corrected"
+target = "Flanker_Standard_Age_Corrected"
 run_training = 1
 
+working_dir = os.getcwd()
+
+datafilename = "final_df_for_xgboost.csv"
+
+# load and clean data
+df = load_and_clean_data(working_dir, datafilename, target)
+
+# plot feature correlation heatmap
+
+plot_title="Correlation between regional volume measures after dividing by totTissue"
+corr_matrix = plot_correlations(df, target, plot_title)
+
+plt.show()
+
 if run_training:
-
-    working_dir = os.getcwd()
-
-    df = pd.read_csv(f"{working_dir}/final_df_for_xgboost.csv")
-
-    columns_to_exclude = ["CandID", "Identifiers", "Combined_ASD_DX", "Risk", "Group", "AB_12_Percent", "AB_24_Percent",
-                       "BRIEF2_GEC_raw_score", "BRIEF2_GEC_T_score", "DCCS_Standard_Age_Corrected"]
-
-    df.drop(columns=columns_to_exclude, inplace=True)
-
-    # Keep only rows where the response variable is not NA
-    df = df[df[output_to_predict].notna()]
-
-    #Encode Sex column Female = 0 Male = 1
-    df["Sex"] = df["Sex"].replace({"Female": 0, "Male": 1})
 
     # Set up 10-fold cross validation
     exp = setup(
         data=df,
-        target=output_to_predict,
-
+        target=target,
         fold=10
     )
 
@@ -42,8 +41,7 @@ if run_training:
         search_library = "optuna",
         search_algorithm="tpe",
         n_iter=50,
-        early_stopping = True,
-        n_jobs=-1
+        early_stopping = True
     )
 
     # Finalize the tuned model (train it on the full dataset)
@@ -52,26 +50,26 @@ if run_training:
     # Predict on the full dataset using the finalized model
     predictions = predict_model(final_model, data=df)
 
-    save_model(final_model, f"{output_to_predict}_final_xgboost_model")
-    predictions.to_csv(f"{output_to_predict}_predictions.csv")
+    save_model(final_model, f"{target}_final_xgboost_model")
+    predictions.to_csv(f"{target}_predictions.csv")
 
 else:
 
-    predictions = pd.read_csv(f"{output_to_predict}_predictions.csv")
-    final_model = load_model(f"{output_to_predict}_final_xgboost_model")
+    predictions = pd.read_csv(f"{target}_predictions.csv")
+    final_model = load_model(f"{target}_final_xgboost_model")
 
 # Plot actual vs. predicted values
-plt.figure(figsize=(8,6))
-sns.scatterplot(x=predictions[output_to_predict] , y=predictions['prediction_label'])
+plt.figure(figsize=(10,8))
+sns.scatterplot(x=predictions[target] , y=predictions['prediction_label'])
 
 # plot y = x line
-plt.plot([min(predictions[output_to_predict]), max(predictions[output_to_predict])],
-         [min(predictions[output_to_predict]), max(predictions[output_to_predict])],
+plt.plot([min(predictions[target]), max(predictions[target])],
+         [min(predictions[target]), max(predictions[target])],
          linestyle = "--", color="red")
 
-plt.xlabel(f"Actual {output_to_predict}")
-plt.ylabel(f"Predicted {output_to_predict}")
-plt.title(f"Predicted vs. Actual {output_to_predict}")
+plt.xlabel(f"Actual {target}")
+plt.ylabel(f"Predicted {target}")
+plt.title(f"Predicted vs. Actual {target}")
 plt.show()
 
 mystop = 1
---
+++ Utility_Functions_XGBoost.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def load_and_clean_data(filepath, filename, target):
+
+    df = pd.read_csv(f"{filepath}/{filename}")
+
+    columns_to_exclude = ["CandID", "Identifiers", "Combined_ASD_DX", "Risk", "Group", "AB_12_Percent", "AB_24_Percent",
+                          "BRIEF2_GEC_raw_score", "BRIEF2_GEC_T_score", "DCCS_Standard_Age_Corrected", "ICV_V12",
+                          "ICV_V24", "totTiss_V12", "totTiss_V24"]
+
+    df.drop(columns=columns_to_exclude, inplace=True)
+
+    # Keep only rows where the response variable is not NA
+    df = df[df[target].notna()]
+
+    # Encode Sex column Female = 0 Male = 1
+    df["Sex"] = df["Sex"].replace({"Female": 0, "Male": 1})
+
+    return df
+
+def plot_correlations(df, target, title):
+    df_features = df.drop(columns=[target])
+    correlation_matrix = df_features.corr()
+
+    # Plot heatmap of correlation coefficients
+    plt.figure(figsize=(11, 8))
+    sns.heatmap(correlation_matrix, cmap='coolwarm', vmin=-1, vmax=1, xticklabels=1, yticklabels=1)
+    # Adjust the margins
+    plt.subplots_adjust(left=0.3, right=0.9, top=0.9, bottom=0.3)
+    plt.xticks(fontsize=8)
+    plt.yticks(fontsize=8)
+    plt.title(title)
+    plt.tight_layout()
+    plt.show()
+
+
