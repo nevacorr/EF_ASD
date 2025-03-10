c40967137db90bd70da414a5245a269d6d858d8b
Neva <nevao@uw.edu>
2025-03-10 13:19:25 -0700

    Remove features with high colinearity

--- IBIS_EF_predict_SA_xgboost.py
+++ IBIS_EF_predict_SA_xgboost.py
@@ -3,7 +3,16 @@ import os
 from pycaret.regression import setup, create_model, tune_model, finalize_model, predict_model, save_model, load_model
 import seaborn as sns
 import matplotlib.pyplot as plt
-from Utility_Functions_XGBoost import load_and_clean_data, plot_correlations
+from Utility_Functions_XGBoost import load_and_clean_data, plot_correlations, remove_collinearity
+from skopt import BayesSearchCV
+from xgboost import XGBRegressor
+from sklearn.model_selection import KFold, StratifiedKFold
+import numpy as np
+import time
+import json
+import pickle
+from sklearn.metrics import mean_squared_error,r2_score
+import logging
 
 target = "Flanker_Standard_Age_Corrected"
 run_training = 1
@@ -16,56 +25,89 @@ datafilename = "final_df_for_xgboost.csv"
 df = load_and_clean_data(working_dir, datafilename, target)
 
 # plot feature correlation heatmap
+# plot_title="Correlation between regional volume measures after dividing by totTissue"
+# corr_matrix = plot_correlations(df, target, plot_title)
+# plt.show()
 
-plot_title="Correlation between regional volume measures after dividing by totTissue"
-corr_matrix = plot_correlations(df, target, plot_title)
+df = remove_collinearity(df, 0.9)
+# plot_title="After removing colinear features"
+# corr_matrix = plot_correlations(df, target, plot_title)
+# plt.show()
 
-plt.show()
+
+# remove features so that none have more than 0.9 correlation with other
 
 if run_training:
 
-    # Set up 10-fold cross validation
-    exp = setup(
-        data=df,
-        target=target,
-        fold=10
-    )
+    X = df.drop(columns=[target]).values
+    y = df[target].values
 
-    # Tune XGBoost model with Bayesian optimization
-    xgb = create_model("xgboost")
+    params = {"n_estimators": (50, 2001),
+              "min_child_weight": (1, 11),
+              "gamma": (0.01, 5.0, "log-uniform"),
+              "eta": (0.005, 0.5, "log-uniform"),
+              "subsample": (0.2, 1.0),
+              "colsample_bytree": (0.2, 1.0),
+              "max_depth": (2, 6), }
 
-    # Tuning the model using Bayesian optimization with Optuna
-    tuned_xgb = tune_model(
-        xgb,
-        optimize="R2",
-        search_library = "optuna",
-        search_algorithm="tpe",
-        n_iter=50,
-        early_stopping = True
-    )
+    xgb = XGBRegressor(objective="reg:squarederror", nthread=16)
+    opt = BayesSearchCV(xgb, params, n_iter=100, n_jobs=-1)
 
-    # Finalize the tuned model (train it on the full dataset)
-    final_model = finalize_model(tuned_xgb)
+    # kf = KFold(n_splits=2, shuffle=True, random_state=42)
+    kf = KFold(n_splits=10, shuffle=True, random_state=42)
 
-    # Predict on the full dataset using the finalized model
-    predictions = predict_model(final_model, data=df)
+    predictions = np.zeros_like(y, dtype=np.float64)
 
-    save_model(final_model, f"{target}_final_xgboost_model")
-    predictions.to_csv(f"{target}_predictions.csv")
+    start_time = time.time()
+    for i, (train_index, test_index) in enumerate(kf.split(X, y)):
+        print(f"Split {i + 1} - Training on {len(train_index)} samples, Testing on {len(test_index)} samples")
+
+        print("fitting")
+        opt.fit(X[train_index], y[train_index])
+        print("predicting")
+        predictions[test_index] = opt.predict(X[test_index])
+
+        end_time = time.time()
+        elapsed_time = (end_time - start_time) / 60.0
+        print(f"Elapsed time for fold {i+1}: {elapsed_time:.2f} minutes")
+
+        # Compute metrics
+        mse = mean_squared_error(y[test_index], predictions[test_index], squared=False)
+        r2 = r2_score(y[test_index], predictions[test_index])
+
+        print(f"Best Parameters for Split {i + 1}: {opt.best_params_}")
+        print(f"Performance for Split {i + 1}: R² = {r2:.4f}, MSE = {mse:.4f}")
+
+        try:
+            # Save model
+            with open(f"{target}_{i+1}_trained_model.pkl", "wb") as f:
+                pickle.dump(opt, f)
+
+            print(f"Trained model saved to {target}_{i+1}_trained_model.pkl")
+
+        except Exception as e:
+            logging.warning(f"An error occurred: {e}")  # Logs error but continues execution
+
+
+    df["Predictions"]  = predictions
+
+    end_time = time.time()
+    elapsed_time = (end_time - start_time)  / 60.0
+    print(f"Computations complete. Time to run all 10 folds: {elapsed_time:.2f} minutes")
+
+    df.to_csv(f"{target}_cv_predictions.csv", index=False)
 
 else:
 
-    predictions = pd.read_csv(f"{target}_predictions.csv")
-    final_model = load_model(f"{target}_final_xgboost_model")
+    df = pd.read_csv(f"{target}_cv_predictions.csv")
 
 # Plot actual vs. predicted values
 plt.figure(figsize=(10,8))
-sns.scatterplot(x=predictions[target] , y=predictions['prediction_label'])
+sns.scatterplot(x=df[target] , y=df["Predictions"])
 
+y=df[target]
 # plot y = x line
-plt.plot([min(predictions[target]), max(predictions[target])],
-         [min(predictions[target]), max(predictions[target])],
-         linestyle = "--", color="red")
+plt.plot([min(y), max(y)], [min(y), max(y)], linestyle = "--", color="red")
 
 plt.xlabel(f"Actual {target}")
 plt.ylabel(f"Predicted {target}")
