e44e9de8308f38d525388d1b1fc2b5413fbe9bad
Neva <nevao@uw.edu>
2025-03-10 17:55:21 -0700

    formatting change

--- IBIS_EF_predict_SA_xgboost.py
+++ IBIS_EF_predict_SA_xgboost.py
@@ -36,63 +36,63 @@ if run_training:
         corr_matrix = plot_correlations(df, target, plot_title)
         plt.show()
 
-        X = df.drop(columns=[target]).values
-        y = df[target].values
+    X = df.drop(columns=[target]).values
+    y = df[target].values
 
-        params = {"n_estimators": (50, 2001),
-                  "min_child_weight": (1, 11),
-                  "gamma": (0.01, 5.0, "log-uniform"),
-                  "eta": (0.005, 0.5, "log-uniform"),
-                  "subsample": (0.2, 1.0),
-                  "colsample_bytree": (0.2, 1.0),
-                  "max_depth": (2, 6), }
+    params = {"n_estimators": (50, 2001),
+              "min_child_weight": (1, 11),
+              "gamma": (0.01, 5.0, "log-uniform"),
+              "eta": (0.005, 0.5, "log-uniform"),
+              "subsample": (0.2, 1.0),
+              "colsample_bytree": (0.2, 1.0),
+              "max_depth": (2, 6), }
 
-        xgb = XGBRegressor(objective="reg:squarederror", nthread=16)
-        opt = BayesSearchCV(xgb, params, n_iter=100, n_jobs=-1)
+    xgb = XGBRegressor(objective="reg:squarederror", nthread=16)
+    opt = BayesSearchCV(xgb, params, n_iter=100, n_jobs=-1)
 
-        kf = KFold(n_splits=10, shuffle=True, random_state=42)
+    kf = KFold(n_splits=10, shuffle=True, random_state=42)
 
-        predictions = np.zeros_like(y, dtype=np.float64)
+    predictions = np.zeros_like(y, dtype=np.float64)
 
-        start_time = time.time()
-        for i, (train_index, test_index) in enumerate(kf.split(X, y)):
-            print(f"Split {i + 1} - Training on {len(train_index)} samples, Testing on {len(test_index)} samples")
+    start_time = time.time()
+    for i, (train_index, test_index) in enumerate(kf.split(X, y)):
+        print(f"Split {i + 1} - Training on {len(train_index)} samples, Testing on {len(test_index)} samples")
 
-            # Fit model to train set
-            print("fitting")
-            opt.fit(X[train_index], y[train_index])
+        # Fit model to train set
+        print("fitting")
+        opt.fit(X[train_index], y[train_index])
 
-            # Use model to predict on test set
-            print("predicting")
-            predictions[test_index] = opt.predict(X[test_index])
+        # Use model to predict on test set
+        print("predicting")
+        predictions[test_index] = opt.predict(X[test_index])
 
-            # Calculate and print elapsed time since program start
-            end_time = time.time()
-            elapsed_time = (end_time - start_time) / 60.0
-            print(f"Elapsed time for fold {i+1}: {elapsed_time:.2f} minutes")
-
-            # Compute performance metrics
-            mse = mean_squared_error(y[test_index], predictions[test_index], squared=False)
-            r2 = r2_score(y[test_index], predictions[test_index])
-
-            print(f"Best Parameters for Split {i + 1}: {opt.best_params_}")
-            print(f"Performance for Split {i + 1}: R² = {r2:.4f}, MSE = {mse:.4f}")
-
-            # Save model to file
-            with open(f"{target}_{i+1}_trained_model.pkl", "wb") as f:
-                pickle.dump(opt, f)
-            print(f"Trained model saved to {target}_{i+1}_trained_model.pkl")
-
-        # Put predictions in a dataframe column
-        df["Predictions"]  = predictions
-
-        # Calculate time it took to complete all predictions across all cv splits
+        # Calculate and print elapsed time since program start
         end_time = time.time()
-        elapsed_time = (end_time - start_time)  / 60.0
-        print(f"Computations complete. Time to run all 10 folds: {elapsed_time:.2f} minutes")
+        elapsed_time = (end_time - start_time) / 60.0
+        print(f"Elapsed time for fold {i+1}: {elapsed_time:.2f} minutes")
 
-        # Save predictions to file
-        df.to_csv(f"{target}_cv_predictions.csv", index=False)
+        # Compute performance metrics
+        mse = mean_squared_error(y[test_index], predictions[test_index], squared=False)
+        r2 = r2_score(y[test_index], predictions[test_index])
+
+        print(f"Best Parameters for Split {i + 1}: {opt.best_params_}")
+        print(f"Performance for Split {i + 1}: R² = {r2:.4f}, MSE = {mse:.4f}")
+
+        # Save model to file
+        with open(f"{target}_{i+1}_trained_model.pkl", "wb") as f:
+            pickle.dump(opt, f)
+        print(f"Trained model saved to {target}_{i+1}_trained_model.pkl")
+
+    # Put predictions in a dataframe column
+    df["Predictions"]  = predictions
+
+    # Calculate time it took to complete all predictions across all cv splits
+    end_time = time.time()
+    elapsed_time = (end_time - start_time)  / 60.0
+    print(f"Computations complete. Time to run all 10 folds: {elapsed_time:.2f} minutes")
+
+    # Save predictions to file
+    df.to_csv(f"{target}_cv_predictions.csv", index=False)
 
 else:
 
