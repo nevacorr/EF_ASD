cd782a7854b19fbacfb98ec6b5a7ec095560ddc3
nevao <nevao@uw.edu>
2025-05-01 17:03:17 -0700

    place bootstrapping within train/test split loop

--- Calc_Distribution_R2_xgboost_ridge.py
+++ Calc_Distribution_R2_xgboost_ridge.py
@@ -1,7 +1,7 @@
 import os
 import matplotlib.pyplot as plt
 from Utility_Functions_XGBoost import plot_correlations, remove_collinearity, plot_xgb_actual_vs_pred
-from Utility_Functions_XGBoost import write_modeling_data_and_outcome_to_file
+from Utility_Functions_XGBoost import write_modeling_data_and_outcome_to_file, generate_bootstrap_indices
 from skopt import BayesSearchCV
 from xgboost import XGBRegressor
 from sklearn.model_selection import KFold
@@ -12,6 +12,11 @@ from sklearn.metrics import mean_squared_error,r2_score
 from load_brain_data import load_subcortical_data, load_and_clean_volume_data, load_and_clean_dti_data
 import itertools
 from neurocombat_sklearn import CombatModel
+import seaborn as sns
+import warnings
+
+# Suppress all FutureWarnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 def load_data(target, metric, include_group_feature, run_dummy_quick_fit, show_heat_map, remove_colinear):
 
@@ -75,10 +80,12 @@ def load_data(target, metric, include_group_feature, run_dummy_quick_fit, show_h
     return X, y, df
 
 # Main xgboost prediction code
-def predict_SA_xgboost(X, y, df, target, metric, params, run_dummy_quick_fit, set_params_man, show_results_plot, bootstrap):
+def predict_SA_xgboost(X, y, df, target, metric, params, run_dummy_quick_fit, set_params_man, show_results_plot, n_bootstraps):
 
     set_parameters_manually = set_params_man
     show_results_plot = show_results_plot
+
+    r2_test_all_bootstraps=[]
 
     # set number of iterations for BayesCV
     n_iter = 100
@@ -101,123 +108,126 @@ def predict_SA_xgboost(X, y, df, target, metric, params, run_dummy_quick_fit, se
             n_estimators=params["n_estimators"],  # Number of trees to create during training
             subsample=params["subsample"]  # Fraction of training dta that is sampled for each boosting round
         )
-
-    # define cross validation scheme
-    kf = KFold(n_splits=10, shuffle=True, random_state=42)
-
-    # make variables to hold predictions for train and test run, as well as counts for how many times each subject appears in a train set
-    train_predictions = np.zeros_like(y, dtype=np.float64)
-    test_predictions = np.zeros_like(y, dtype=np.float64)
-    train_counts = np.zeros_like(y, dtype=np.int64)
-
     # record start time
     start_time = time.time()
-    # make indexes for train/test subjects for each fold
-    for i, (train_index, test_index) in enumerate(kf.split(X, y)):
-        print(f"{metric} Split {i + 1} - Training on {len(train_index)} samples, Testing on {len(test_index)} samples")
+    for b in range(n_bootstraps):
+        print(f"Bootstrap iteration {b + 1}/{n_bootstraps}")
 
-        # initialize combat model
-        combat = CombatModel()
+        # define cross validation scheme
+        kf = KFold(n_splits=10, shuffle=True, random_state=42)
 
-        X_train = X.iloc[train_index].copy()
-        X_test = X.iloc[test_index].copy()
+        # make variables to hold predictions for train and test run, as well as counts for how many times each subject appears in a train set
+        train_predictions = np.zeros_like(y, dtype=np.float64)
+        test_predictions = np.zeros_like(y, dtype=np.float64)
+        train_counts = np.zeros_like(y, dtype=np.int64)
 
-        # Create Categorical object for the training set sites
-        train_sites = pd.Categorical(X_train['Site'])
+        # make indexes for train/test subjects for each fold
+        for i, (train_index, test_index) in enumerate(kf.split(X, y)):
+            print(f"bootstrap={b}/{n_bootstraps} {metric} Split {i + 1} - Training on {len(train_index)} samples, Testing on {len(test_index)} samples")
 
-        # Convert training sites to numeric codes (for harmonization)
-        sites_train = train_sites.codes
+            # initialize combat model
+            combat = CombatModel()
 
-        # Replace the 'Site' column in X_train with the codes
-        X_train['Site'] = sites_train
+            X_train = X.iloc[train_index].copy()
+            y_train = y[train_index].copy()
+            X_test = X.iloc[test_index].copy()
+            y_test = y[test_index].copy()
 
-        # Apply the same categorical mapping to the test set sites
-        test_sites = pd.Categorical(X_test['Site'], categories=train_sites.categories)
-        sites_test = test_sites.codes
+            # Bootstrap the training data only
+            bootstrap_indices = np.random.choice(len(train_index), size=len(train_index), replace=True)
+            X_train_boot = X_train.iloc[bootstrap_indices].reset_index(drop=True)
+            y_train_boot = y_train.iloc[bootstrap_indices].reset_index(drop=True)
 
-        # Replace the 'Site' column in X_test with the codes
-        X_test['Site'] = sites_test
+            # Create Categorical object for the training set sites
+            train_sites = pd.Categorical(X_train_boot['Site'])
 
-        #  Replace NaN values with column mean for harmonization
-        # Drop the 'Site' column because X_train and X_test after running combat will not have this column
-        nan_indices_train = X_train.isna().drop(columns=['Site','Sex'])
-        nan_indices_test = X_test.isna().drop(columns=['Site', 'Sex'])
-        X_train_temp = X_train.copy()  # Create a copy of the training data to avoid modifying original data
-        X_test_temp = X_test.copy()
-        X_train_temp = X_train_temp.fillna(X_train_temp.mean()) # Replace NaN with the column mean (change to use scikit learn)
-        X_test_temp = X_test_temp.fillna(X_train_temp.mean()) # Replace NaN with the column mean of the train set
+            # Convert training sites to numeric codes (for harmonization)
+            sites_train = train_sites.codes
 
-        # Keep a copy of Sex
-        sex_train = X_train_temp['Sex'].values.reshape(-1,1)
-        sex_test = X_test_temp['Sex'].values.reshape(-1, 1)
+            # Replace the 'Site' column in X_train with the codes
+            X_train_boot['Site'] = sites_train
 
-        # Harmonize the training data
-        X_train_combat = combat.fit_transform(X_train_temp.drop(columns=['Site', 'Sex']), sites_train.reshape(-1, 1))
-        # Replace the original Nan values
-        X_train_combat[nan_indices_train] = np.nan
+            # Apply the same categorical mapping to the test set sites
+            test_sites = pd.Categorical(X_test['Site'], categories=train_sites.categories)
+            sites_test = test_sites.codes
 
-        # Harmonize the test data (using the same harmonization model fitted on the training data)
-        X_test_combat = combat.transform(X_test_temp.drop(columns=['Site', 'Sex']), sites_test.reshape(-1, 1))
-        # Replace the original Nan values
-        X_test_combat[nan_indices_test] = np.nan
+            # Replace the 'Site' column in X_test with the codes
+            X_test['Site'] = sites_test
 
-        # Add sex values back into array for xgboost now that the brain measures have been harmonized
-        X_train_combat = np.hstack([sex_train,X_train_combat])
-        X_test_combat = np.hstack([sex_test, X_test_combat])
+            #  Replace NaN values with column mean for harmonization
+            # Drop the 'Site' column because X_train and X_test after running combat will not have this column
+            nan_indices_train = X_train_boot.isna().drop(columns=['Site','Sex'])
+            nan_indices_test = X_test.isna().drop(columns=['Site', 'Sex'])
+            X_train_boot_temp = X_train_boot.copy()  # Create a copy of the training data to avoid modifying original data
+            X_test_temp = X_test.copy()
+            X_train_temp = X_train_boot_temp.fillna(X_train_boot_temp.mean()) # Replace NaN with the column mean (change to use scikit learn)
+            X_test_temp = X_test_temp.fillna(X_train_temp.mean()) # Replace NaN with the column mean of the train set
+
+            # Keep a copy of Sex
+            sex_train = X_train_boot_temp['Sex'].values.reshape(-1,1)
+            sex_test = X_test_temp['Sex'].values.reshape(-1, 1)
+
+            # Harmonize the training data
+            X_train_boot_combat = combat.fit_transform(X_train_boot_temp.drop(columns=['Site', 'Sex']), sites_train.reshape(-1, 1))
+            # Replace the original Nan values
+            X_train_boot_combat[nan_indices_train] = np.nan
+
+            # Harmonize the test data (using the same harmonization model fitted on the training data)
+            X_test_combat = combat.transform(X_test_temp.drop(columns=['Site', 'Sex']), sites_test.reshape(-1, 1))
+            # Replace the original Nan values
+            X_test_combat[nan_indices_test] = np.nan
+
+            # Add sex values back into array for xgboost now that the brain measures have been harmonized
+            X_train_boot_combat = np.hstack([sex_train,X_train_boot_combat])
+            X_test_combat = np.hstack([sex_test, X_test_combat])
+
+            if set_parameters_manually == 0:
+                # Fit model to train set
+                print("fitting")
+                opt.fit(X_train_boot_combat, y_train_boot)
+
+                # Use model to predict on test set
+                test_predictions[test_index] = opt.predict(X_test_combat)
+
+                # Predict for train set
+                train_predictions[train_index] += opt.predict(X_train_boot_combat)
+
+            else:
+                # Fit xgboost model using specified parameters
+                xgb.fit(X_train_boot_combat, y_train_boot)
+                test_predictions[test_index] = xgb.predict(X_test_combat)
+                train_predictions[train_index] += xgb.predict(X_train_boot_combat)
+
+            # Keep track of the number of times that each subject is included in the train set
+            train_counts[train_index] += 1
+
+        # Correct the predictions for the train set by the number of times they appeared in the train set
+        train_predictions /= train_counts
 
         if set_parameters_manually == 0:
-            # Fit model to train set
-            print("fitting")
-            opt.fit(X_train_combat, y[train_index])
+            best_params = opt.best_params_
+            print(f"Best Parameters for Final {metric} Model: {best_params}")
+        elif set_parameters_manually == 1:
+            best_params = params
 
-            # Use model to predict on test set
-            test_predictions[test_index] = opt.predict(X_test_combat)
+        # Compute R2
+        r2_test = r2_score(y_test, test_predictions)
+        r2_train = r2_score(y_train_boot, train_predictions)
 
-            # Predict for train set
-            train_predictions[train_index] += opt.predict(X_train_combat)
+        print(f"Final performance. R2train = {r2_train:.3f} R2test = {r2_test:.3f}")
 
-        else:
-            # Fit xgboost model using specified parameters
-            xgb.fit(X_train_combat, y[train_index])
-            test_predictions[test_index] = xgb.predict(X_test_combat)
-            train_predictions[train_index] += xgb.predict(X_train_combat)
+        # Calculate and print time it took to complete all model creations and predictions across all cv splits
+        end_time = time.time()
+        elapsed_time = (end_time - start_time) / 60.0
+        print(f"bootstrap{b}/{n_bootstraps} complete. Time to run this bootstrap: {elapsed_time:.2f} minutes")
 
-        # Keep track of the number of times that each subject is included in the train set
-        train_counts[train_index] += 1
+        # write_modeling_data_and_outcome_to_file(run_dummy_quick_fit, metric, params, set_parameters_manually, target, df,
+        #                                         r2_train, r2_test, best_params, bootstrap, elapsed_time)
 
-        # Calculate and print time it took to run this fold
-        fold_time = time.time()
-        elapsed_time = (fold_time - start_time) / 60.0
-        print(f"{metric}  Time elapsed: {elapsed_time:.2f} minutes")
+        # plot_xgb_actual_vs_pred(metric, target, r2_train, r2_test, df, best_params, show_results_plot)
+        r2_test_all_bootstraps.append(r2_test)
 
-    # Correct the predictions for the train set by the number of times they appeared in the train set
-    train_predictions /= train_counts
-
-    # Put train and test_predictions in a dataframe
-    df["test_predictions"]  = test_predictions
-    df["train_predictions"] = train_predictions
-
-    # Calculate and print time it took to complete all model creations and predictions across all cv splits
-    end_time = time.time()
-    elapsed_time = (end_time - start_time)  / 60.0
-    print(f"{metric} Computations complete. Time to run all 10 folds: {elapsed_time:.2f} minutes")
-
-    if set_parameters_manually == 0:
-        best_params = opt.best_params_
-        print(f"Best Parameters for Final {metric} Model: {best_params}")
-    elif set_parameters_manually == 1:
-        best_params = params
-
-    # Compute R2
-    r2_test = r2_score(df[target], df["test_predictions"])
-    r2_train = r2_score(df[target], df["train_predictions"])
-
-    print(f"Final performance. R2train = {r2_train:.3f} R2test = {r2_test:.3f}")
-
-    write_modeling_data_and_outcome_to_file(run_dummy_quick_fit, metric, params, set_parameters_manually, target, df,
-                                            r2_train, r2_test, best_params, bootstrap, elapsed_time)
-
-    plot_xgb_actual_vs_pred(metric, target, r2_train, r2_test, df, best_params, show_results_plot)
+    return r2_test_all_bootstraps
 
  ######## End of function predict_SA_xgboost#######
  #####################################################################################################################
@@ -225,6 +235,7 @@ def predict_SA_xgboost(X, y, df, target, metric, params, run_dummy_quick_fit, se
 target = "BRIEF2_GEC_T_score"
 metric = 'subcort'
 include_group = 1
+n_bootstraps = 10
 
 # Define parameter ranges to be used (ranges if BayesCV will be used)
 params = {"n_estimators": 50,  # (50, 2001),# Number of trees to create during training
@@ -240,22 +251,42 @@ params = {"n_estimators": 50,  # (50, 2001),# Number of trees to create during t
 
 run_dummy_quick_fit = 0
 
-X, y, df = load_data(
-            target,
-            metric,
-            include_group,
-            run_dummy_quick_fit,
-            show_heat_map=0,
-            remove_colinear=0,
-)
-predict_SA_xgboost(
-            X,
-            y,
-            df,
-            target,
-            metric,
-            params,
-            run_dummy_quick_fit,
-            set_params_man=1,
-            show_results_plot=0,
-            bootstrap=0)
+X, y, df = load_data(target, metric, include_group, run_dummy_quick_fit, show_heat_map=0, remove_colinear=0)
+
+r2_test_xgboost = []
+r2_test = predict_SA_xgboost(X, y, df, target, metric, params, run_dummy_quick_fit, set_params_man=1,
+                show_results_plot=0, bootstrap=1)
+r2_test_xgboost.append(r2_test)
+
+r2_test_array_xgb = np.array(r2_test_xgboost)
+
+alpha=5
+
+lower_bound = np.percentile(r2_test_array_xgb, alpha)
+
+if lower_bound > 0:
+    print(f"R² is significantly greater than 0 at the 5% level (one-tailed)")
+else:
+    print(f"R² is NOT significant at the 5% level (one-tailed)")
+
+print(f"{100 - alpha}% lower confidence bound: {lower_bound:.4f}")
+
+percentile_value = np.percentile(r2_test_array_xgb, alpha)
+
+# Plot the distribution
+plt.figure(figsize=(10, 6))
+sns.histplot(r2_test_array_xgb, bins=30, kde=True, color='skyblue')
+
+# Add vertical line at the 5th percentile
+plt.axvline(percentile_value, color='red', linestyle='--', linewidth=2, label=f'5th percentile = {percentile_value:.3f}')
+
+# Add title and labels
+plt.title(f'Bootstrap R² Distribution with {alpha}% Percentile Marked')
+plt.xlabel('R²')
+plt.ylabel('Frequency')
+
+# Add legend
+plt.legend()
+
+# Show plot
+plt.show()
