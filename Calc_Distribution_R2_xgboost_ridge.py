e640e2c34993932a236187866b1dc75f080cf8dd
nevao <nevao@uw.edu>
2025-06-05 13:13:31 -0700

    include flag for manual set of xgb parameters vs gridsearch

--- Calc_Distribution_R2_xgboost_ridge.py
+++ Calc_Distribution_R2_xgboost_ridge.py
@@ -8,10 +8,10 @@ from predict_SA_ridge import predict_SA_ridge
 from create_predictor_target_vars import create_predictor_target_vars
 
 target = "BRIEF2_GEC_T_score"
-metric = 'ad_VSA'
+metric = 'subcort_infant'
 #options 'volume_infant', 'volume_VSA', 'subcort_VSA', 'subcort_infant', 'ad_VSA', 'rd_VSA', 'md_VSA', 'fa_VSA'
 #        'surface_area_infant
-include_group = 0
+include_group = 1
 n_bootstraps = 1
 show_heat_map = 0
 remove_colinear = 0
@@ -20,18 +20,30 @@ alpha=0.05
 
 run_ridge_regression_fit = 1
 run_xgboost_fit = 0
+set_xgb_params_man = 0
 
-# Define parameter ranges to be used (ranges if BayesCV will be used)
-params = {"n_estimators": 353,  # (50, 2001),# Number of trees to create during training
-          "min_child_weight": 11,
-          # (1,11) # the number of samples required in each child node before attempting to split further
-          "gamma": 5,
-          # (0.01, 5.0, "log-uniform"),# regularization. Low values allow splits as long as they improve the loss function, no matter how small
-          "eta": 0.011875,  # (0.005, 0.5, "log-uniform"),# learning rate
-          "subsample": 1.0,  # (0.2, 1.0),# Fraction of training dta that is sampled for each boosting round
-          "colsample_bytree": 1.0,  # (0.2, 1.0)  the fraction of features to be selected for each tree
-          "max_depth": 3  # (2, 6), }#maximum depth of each decision tree
-          }
+if set_xgb_params_man:
+    # Define parameter ranges to be used (ranges if BayesCV will be used)
+    params = {"n_estimators": 353,  # (50, 2001),# Number of trees to create during training
+              "min_child_weight": 11,
+              # (1,11) # the number of samples required in each child node before attempting to split further
+              "gamma": 5,
+              # (0.01, 5.0, "log-uniform"),# regularization. Low values allow splits as long as they improve the loss function, no matter how small
+              "eta": 0.011875,  # (0.005, 0.5, "log-uniform"),# learning rate
+              "subsample": 1.0,  # (0.2, 1.0),# Fraction of training dta that is sampled for each boosting round
+              "colsample_bytree": 1.0,  # (0.2, 1.0)  the fraction of features to be selected for each tree
+              "max_depth": 3  # (2, 6), }#maximum depth of each decision tree
+              }
+else:
+    # Define parameter ranges to be used (ranges if BayesCV will be used)
+    params = {"n_estimators": (50, 2001),# Number of trees to create during training
+              "min_child_weight": (1,11), # the number of samples required in each child node before attempting to split further
+              "gamma": (0.01, 5.0, "log-uniform"),# regularization. Low values allow splits as long as they improve the loss function, no matter how small
+              "eta": (0.005, 0.5, "log-uniform"),# learning rate
+              "subsample": (0.2, 1.0),# Fraction of training dta that is sampled for each boosting round
+              "colsample_bytree": (0.2, 1.0), # the fraction of features to be selected for each tree
+              "max_depth": (2, 6) #maximum depth of each decision tree
+              }
 
 # Load and clean data for selected target and metric
 df = load_all_data()
@@ -44,7 +56,7 @@ print(f"Running with target = {target} metric = {metric} include_group = {includ
 
 if run_xgboost_fit:
     # Use XGBoost to predict school age behavior from brain metric
-    r2_test_array_xgb = predict_SA_xgboost(X, y, df, target, metric, params, run_dummy_quick_fit_xgb, set_params_man=1,
+    r2_test_array_xgb = predict_SA_xgboost(X, y, target, metric, params, run_dummy_quick_fit_xgb, set_xgb_params_man,
                     show_results_plot=0, n_bootstraps=n_bootstraps)
 
     # Calculate_xgb_percentile for r2test
@@ -60,7 +72,7 @@ if run_ridge_regression_fit:
     best_ridge_alpha = tune_ridge_alpha(X, y)
 
     # Run ridge regression
-    r2_test_array_ridge, coef_summary_ridge = predict_SA_ridge(X, y, df, target, best_ridge_alpha, n_bootstraps)
+    r2_test_array_ridge, coef_summary_ridge = predict_SA_ridge(X, y, target, best_ridge_alpha, n_bootstraps)
 
     # Calculate ridge_percentile for r2test
     result_text_ridge, percentile_value_ridge = calculate_percentile(r2_test_array_ridge, alpha)
